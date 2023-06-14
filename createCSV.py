1b8416e7db970d35a2bceb3e15d8b23a9b701712
noggl <noggl@users.noreply.github.com>
2023-06-14 17:10:12 -0400

    Remove submodules

--- createCSV.py
+++ createCSV.py
@@ -166,10 +166,8 @@ def main():
                         print("Added " + show["title"] + " (" + str(show["anilist_id"]) + ") Season " + str(show["animeSeason"]))
                     elif show["type"] == "MOVIE":
                         print("Added " + show["title"] + " (" + str(show["anilist_id"]) + ")")
-        return 0
     else:
         print("No changes, exiting")
-        return 0
 
 
 if __name__ == "__main__":
